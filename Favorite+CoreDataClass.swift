37d13bc593e24768cee3550d9f6cd5f889105e10
tsapuanq <sapuantalaspai@gmail.com>
2025-11-22 17:36:42 +0500

    Refactor Core Data logic

---
+++ Favorite+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Favorite+CoreDataClass.swift
+//  MovieDB-CSS214
+//
+//  Created by Sapuan Talaspay on 11/22/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Favorite)
+public class Favorite: NSManagedObject {
+
+}
